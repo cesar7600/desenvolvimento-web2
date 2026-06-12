8e0c090749458aaaa66752e08512c2a1f0ac527e
cesar7600 <juliorodriguessoares26@gmail.com>
2026-06-11 21:42:00 -0300

    check lista

---
+++ 007/script.js
@@ -0,0 +1,9 @@
+function cadastro() {
+    console.log('teste')
+    let pnome = document.getElementById('pnome').value;
+    let resuldado = document.getElementById;
+    let 
+    window.alert(`${pnome} seu cadastro foi realizado com sucesso!`);
+    resuldado.innerHTML = `<p>Sr. ${pnome}, seu cadastro foi realizado com sucesso!</p>`
+    resuldado.style.backgroundColor = 'red'
+}
