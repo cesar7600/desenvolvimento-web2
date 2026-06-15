d7959c5aaa2762ab573fa94ca42347a57c82a760
cesar7600 <juliorodriguessoares26@gmail.com>
2026-06-15 19:09:01 -0300

    Revert "check lista"
    This reverts commit 8e0c090749458aaaa66752e08512c2a1f0ac527e.

--- 007/script.js
+++
@@ -1,9 +0,0 @@
-function cadastro() {
-    console.log('teste')
-    let pnome = document.getElementById('pnome').value;
-    let resuldado = document.getElementById;
-    let 
-    window.alert(`${pnome} seu cadastro foi realizado com sucesso!`);
-    resuldado.innerHTML = `<p>Sr. ${pnome}, seu cadastro foi realizado com sucesso!</p>`
-    resuldado.style.backgroundColor = 'red'
-}
